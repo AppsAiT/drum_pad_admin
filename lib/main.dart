89196253ce303da8e0f6b8b7c7562038979e565a
Vikram <mamundivikram@gmail.com>
2023-06-09 21:15:03 +0530

    filter bug fix try

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-import 'package:drum_pad_admin/pages/addSong.dart';
-import 'package:drum_pad_admin/pages/allSongsPage.dart';
 import 'package:drum_pad_admin/pages/loginScreen.dart';
-import 'package:drum_pad_admin/pages/membershipPage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'pages/homePage.dart';
-import 'pages/newSampleUpload.dart';
+
+import 'pages/allSongsPage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -39,10 +36,10 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.cyan,
       ),
-      home: const LoginPage(),
+      // home: const LoginPage(),
       // home: const MyHomePage(title: 'DRUM PAD ADMIN'),
       // home: const UploadImage(),
-      // home: const AllSongsPage(),
+      home: const AllSongsPage(user: 'null'),
       // home: const MembershipPage(),
     );
   }
